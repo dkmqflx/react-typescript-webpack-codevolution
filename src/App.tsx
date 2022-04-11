32460feaa3bcbaaa0531b704fb3d2884b9de3a0d
dkmqflx <dkmqflx@gmail.com>
2022-04-11 22:26:36 +0900

    handle png, svg files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import './styles.css';
+import IMAGE from './react.png';
+import LOGO from './logo.svg';
 
 export const App = () => {
-  return <h1>App</h1>;
+  return (
+    <>
+      <h1>App</h1>;
+      <img src={IMAGE} alt="React Logo" />
+      <img src={LOGO} alt="React Logo Svg" width="300" height="200" />
+    </>
+  );
 };
