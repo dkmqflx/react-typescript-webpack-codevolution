5b2379ef01b7510e1fda52c372898828f02f4fb6
dkmqflx <dkmqflx@gmail.com>
2022-04-11 22:49:07 +0900

    divide and merge webpack files depending on env

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,23 @@ import LOGO from './logo.svg';
 export const App = () => {
   return (
     <>
-      <h1>App</h1>;
+      <h1>
+        App - {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      ;
       <img src={IMAGE} alt="React Logo" />
       <img src={LOGO} alt="React Logo Svg" width="300" height="200" />
     </>
   );
 };
+
+/*
+build 환경은 아래 명령어로 실행할 수 있다.
+
+cd build
+
+npx serve 
+
+dev 서버와 prod 서버의 개발자도구 - Network를 통해서 
+bundle.js를 보면 확연히 prod 의 bundle.js 사이즈가 더 작은 것을 알 수 있다
+*/
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,44 +1,11 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// merge webpack.common.js with webpack.dev.js or webpack.prod.js
 
-module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
-      },
-    ],
-  },
-  output: {
-    path: path.resolve(__dirname, '..', './build'),
-    filename: 'bundle.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './src/index.html'),
-    }),
-  ],
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common.js');
+
+module.exports = (envVars) => {
+  const { env } = envVars; // env는 package.json의 start, build의 env= 에 오는 값이 된다.
+  const envConfig = require(`./webpack.${env}.js`);
+  const config = merge(commonConfig, envConfig);
+  return config;
 };
