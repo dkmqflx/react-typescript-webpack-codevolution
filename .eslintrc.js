a51e05156de44d13d0eb14289c3aea2a0cae3e04
Dohyun Kim <42763164+dkmqflx@users.noreply.github.com>
2022-04-13 15:07:49 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,6 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
   extends: [
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
